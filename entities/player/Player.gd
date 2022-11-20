e2e8a6be463fcf34e685a3f2789a420b44194d9d
TheDeafCreeper <34897909+TheDeafCreeper@users.noreply.github.com>
2022-11-20 12:35:16 -0600

    Fixed naming

--- entities/player/Player.gd
+++ entities/player/Player.gd
@@ -2,33 +2,31 @@ extends CharacterBody2D
 class_name Player
 
 # Exported Variables
-@export var MOVE_SPEED: float = 200;
+@export var move_speed: float = 200
 
 # OnReady Variables
-@onready var NAVIGATION: NavigationAgent2D = $NavigationAgent2D;
+@onready var NAVIGATION: NavigationAgent2D = $NavigationAgent2D # ALL_CAPS because it's technically constant.
 
 # Other Variables
-var last_delta: float = 0;
+var last_delta: float = 0
 
 func _ready():
-	NAVIGATION.set_target_location(position);
+	NAVIGATION.set_target_location(position)
 	
 func _physics_process(_delta):
-	last_delta = _delta;
-	var target_position: Vector2 = NAVIGATION.get_next_location();
+	last_delta = _delta
+	var target_position: Vector2 = NAVIGATION.get_next_location()
 	# Get Inputs
 	if Input.is_action_just_pressed("move"):
-		NAVIGATION.set_target_location(get_global_mouse_position());
+		NAVIGATION.set_target_location(get_global_mouse_position())
 		target_position = get_global_mouse_position()
 	
-	var moving = !NAVIGATION.is_navigation_finished();
-	
-	if moving:
-		NAVIGATION.set_velocity(position.direction_to(target_position) * MOVE_SPEED)
+	if !NAVIGATION.is_navigation_finished():
+		NAVIGATION.set_velocity(position.direction_to(target_position) * move_speed)
 
 func _safe_velocity_computed(safe_velocity):
 	velocity = safe_velocity;
-	if position.distance_to(NAVIGATION.get_next_location()) < MOVE_SPEED * last_delta:
+	if position.distance_to(NAVIGATION.get_next_location()) < move_speed * last_delta:
 		position = NAVIGATION.get_next_location()
 	else:
 		move_and_slide()
