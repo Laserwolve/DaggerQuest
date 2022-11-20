f2ad09e0b8a400f083876b9341b2d4787641fec3
TheDeafCreeper <34897909+TheDeafCreeper@users.noreply.github.com>
2022-11-20 14:38:54 -0600

    Added comments

--- scripts/loadAnimations.gd
+++ scripts/loadAnimations.gd
@@ -60,8 +60,8 @@ func _ready():
 	if folder_name == "":
 		return
 
+	# Loads all the files in the designated path (should be the frames)
 	var files: Array[String] = []
-		
 	var dir: DirAccess = DirAccess.open(folder_name)
 	if dir:
 		dir.list_dir_begin()
@@ -73,15 +73,19 @@ func _ready():
 	else:
 		print("An error occurred when trying to access the path.")
 	
+	# Sorts the files so there's a reliable order.
 	files.sort()
 	
+	# Setup the SpriteFrames
 	var spriteFrame: SpriteFrames = SpriteFrames.new()
-
 	for animName in ANIMATONS:
 		spriteFrame.add_animation(animName)
 		spriteFrame.set_animation_speed(animName, 12)
 	
+	# Reverse the list for performance
 	files.reverse()
+	
+	# Add all the frames
 	for animName in ANIMATONS:
 		for i in range(0, 160):
 			spriteFrame.add_frame(animName, ResourceLoader.load(files.pop_back(), "Texture2D", ResourceLoader.CACHE_MODE_IGNORE))
@@ -90,6 +94,7 @@ func _ready():
 	
 	var animationPlayer = AnimationPlayer.new()
 	
+	# Create the animation libraries.
 	for animName in ANIMATONS:
 		var frame_offset = 0
 		var animationLibrary: AnimationLibrary = AnimationLibrary.new()
@@ -109,5 +114,9 @@ func _ready():
 			animationLibrary.add_animation(direction, ani)
 	
 		animationPlayer.add_animation_library(animName, animationLibrary)
+		
+	# Set just a default animation
 	animationPlayer.play("idle/0")
+	
+	# Add the animation player to the Player
 	get_parent().call_deferred("add_child", animationPlayer)
