00975aedbf455d63615e1e8f448ee0fdcce190ab
Daniel <37811619+dhav211@users.noreply.github.com>
2022-11-22 18:40:22 -0800

    item class is created on press of s key, but useless

--- entities/equipment/item_manager.gd
+++ entities/equipment/item_manager.gd
@@ -4,22 +4,29 @@ class_name ItemManager
 func _ready():
 	pass # Replace with function body.
 
+# Create an item from a string value, returns the item from a dictionary value
 static func create_item(item_name : String) -> Item: # Probably don't use string in future but an enum
-	var equipment : Dictionary = {}
-	return null
+	# TODO this dictionary is kinda rough, lets figure out a json solution that is clean to edit outside of here
+	var equipment : Dictionary = {
+		"simple_sword" : Item.new("Simple Sword", "A sword that kills things", "res://assets/sprites/ui/equipment_icons/sword.png", []), 
+		"leggings" : Item.new("Leggings", "Makes your ass look great", "res://assets/sprites/ui/equipment_icons/pants.png", []) }
+	return equipment[item_name]
 
 
 class Item:
-	func _init(item_name : String, icon_path : String, item_mods : Array[ItemMod]):
+	func _init(item_name : String, item_description : String, icon_path : String, item_mods : Array[ItemMod]):
 		self.item_name = item_name
+		self.item_description = item_description
 		self.icon_path = icon_path
 		self.item_mods = item_mods
 
-	var item_name : String = "" 
+	var item_name : String = ""
+	var item_description : String = ""
 	var icon_path : String = "" # asset path to load texture for item
 	var item_mods : Array[ItemMod] = [] # list of modications that effect item
 
 
+# TODO lets move this to its own script, this is currently a proof concept
 class ItemMod:
 	var mod_name : String = "" # this will just be used for ease of access, should probably be replaced with enum in future
 	var description : String = ""  # this will be displayed in item stat window
--- entities/ui/inventory.gd
+++ entities/ui/inventory.gd
@@ -1,9 +1,24 @@
 extends Control
 
+@onready var slots : GridContainer = get_node("Slots")
 
 func _input(event):
 	# Create a random piece of equipment, this is temporary of course
-	if Input.is_key_pressed(KEY_S):
-		var random_equipment : Array[String] = ["Simple Sword", "Leggings"]
-		var equipment : ItemManager.Item = ItemManager.create_item(random_equipment[randi_range(0, random_equipment.size())])
+	if Input.is_action_just_pressed("add_item"):
+		var random_equipment : Array[String] = ["simple_sword", "leggings"]
+		var equipment : ItemManager.Item = ItemManager.create_item(random_equipment[randi_range(0, random_equipment.size() - 1)])
+		add_item_to_first_empty_slot(equipment)
+
+
+func add_item_to_first_empty_slot(item : ItemManager.Item) -> void:
+	var has_found_slot : bool = false
+	print(item.item_name)
+	for slot in slots.get_children():
+		if not slot.is_filled:
+			# add item to inventory
+			pass
+	
+	if not has_found_slot:
+		# Display message that inventory is full
+		pass
 
