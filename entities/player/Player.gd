8b9f4cb669f8d6cff256bc81cd717a5b7cdb6179
TheDeafCreeper <34897909+TheDeafCreeper@users.noreply.github.com>
2022-11-23 01:06:24 -0600

    Removed debug toggle_legs function

--- entities/player/Player.gd
+++ entities/player/Player.gd
@@ -171,18 +171,3 @@ func get_look_angle(look_direction: Vector2) -> String:
 		string = "0"
 	
 	return string
-
-func toggle_legs():
-	if EQUIPMENT_NODES[EquipmentSlots.LEGS].get_child_count() == 0:
-		var legs = EQUIPED_ITEM.instantiate()
-		var legs_shadow = EQUIPED_ITEM.instantiate()
-		legs.folder_name = Items.items[Items.ItemID.SIMPLE_LEGGINGS].equipment_path
-		legs_shadow.folder_name = Items.items[Items.ItemID.SIMPLE_LEGGINGS].shadow_path
-		
-		EQUIPMENT_NODES[EquipmentSlots.LEGS].add_child(legs)
-		SHADOW_NODES[EquipmentSlots.LEGS].add_child(legs_shadow)
-	else:
-		for child in EQUIPMENT_NODES[EquipmentSlots.LEGS].get_children():
-			child.delete()
-		for child in SHADOW_NODES[EquipmentSlots.LEGS].get_children():
-			child.delete()
