55424163546dae8fd673596f864b4c48bf2e782a
TheDeafCreeper <34897909+TheDeafCreeper@users.noreply.github.com>
2022-11-23 22:19:44 -0600

    Switched to ItemIDs

--- entities/equipment/item_manager.gd
+++ entities/equipment/item_manager.gd
@@ -4,22 +4,39 @@ class_name ItemManager
 func _ready():
 	pass # Replace with function body.
 
-# Create an item from a string value, returns the item from a dictionary value
-static func create_item(item_name : String) -> Item: # Probably don't use string in future but an enum
+# Define Item IDs
+enum ItemId {
+	SIMPLE_SWORD,
+	LEGGINGS
+}
+
+# Lets you get the string value of an ID
+func get_item_name_from_id(item_id: ItemId) -> String:
+	return ItemId.keys()[item_id]
+
+# Create an item from an ItemId, returns the item from a dictionary value
+static func create_item(item_id : ItemId) -> Item:
 	# TODO this dictionary is kinda rough, lets figure out a json solution that is clean to edit outside of here
 	var equipment : Dictionary = {
-		"simple_sword" : Item.new("Simple Sword", "A sword that kills things", "res://assets/sprites/ui/equipment_icons/sword.png", []), 
-		"leggings" : Item.new("Leggings", "Makes your ass look great", "res://assets/sprites/ui/equipment_icons/pants.png", []) }
-	return equipment[item_name]
+		ItemId.SIMPLE_SWORD : Item.new(ItemId.SIMPLE_SWORD, "Simple Sword", "A sword that kills things", "res://assets/sprites/ui/equipment_icons/sword.png", []), 
+		ItemId.LEGGINGS : Item.new(ItemId.LEGGINGS, "Leggings", "Makes your ass look great", "res://assets/sprites/ui/equipment_icons/pants.png", []) }
+	return equipment[item_id]
 
 
 class Item:
-	func _init(item_name : String, item_description : String, icon_path : String, item_mods : Array[ItemMod]):
+	func _init(
+		item_id: ItemId,
+		item_name : String,
+		item_description : String,
+		icon_path : String,
+		item_mods : Array[ItemMod]):
+		self.item_id = item_id
 		self.item_name = item_name
 		self.item_description = item_description
 		self.icon_path = icon_path
 		self.item_mods = item_mods
 
+	var item_id: ItemId = null
 	var item_name : String = ""
 	var item_description : String = ""
 	var icon_path : String = "" # asset path to load texture for item
