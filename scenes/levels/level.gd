39e486ad4e53bc55f6c656418f6f02bceb8e38a5
TheDeafCreeper <34897909+TheDeafCreeper@users.noreply.github.com>
2022-11-24 22:44:36 -0600

    Added create_loot function to level script

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,7 +1,16 @@
 extends Node2D
 class_name Level
 
+const LOOT_INSTANCE = preload("res://entities/loot/Loot.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Global.level = self
+
+func create_loot(item: ItemManager.Item, pos: Vector2):
+	print("Created item")
+	var loot = LOOT_INSTANCE.instantiate()
+	loot.item = item
+	loot.position = pos
+	
+	$Loot.add_child(loot)
